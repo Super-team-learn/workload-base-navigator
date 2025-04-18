b58a1beeb58f8e0e072680a1569e733975d26689
Khokhlov Kirill <hkp69@yandex.ru>
2025-04-19 02:32:40 +0300

    feat: connect to real database

--- save_in_db.py
+++ save_in_db.py
@@ -1,23 +1,23 @@
 import requests
 import psycopg2
 import time
-from datetime import datetime
+import datetime
 
 conn = psycopg2.connect(
-    dbname="dbname",
-    user="username",
-    password="password",
-    host="localhost"
+    dbname="count_of_people_on_stations",
+    user="hackathon",
+    password="hackathon",
+    host="khokhlovkirill.ru"
 )
 cursor = conn.cursor()
 all_stations = ['1', '2', '3', '4', '5', '6']
 while True:
     for station in all_stations:
         num_people = requests.post('http://127.0.0.1:8001/count_people', json={'station_name': station}).json()
-        num_people = num_people['num_people']
-        now = datetime.now()
-        cursor.execute("INSERT INTO users (station_name, time, num_people) VALUES (%s, %s, %s)", (station, now, num_people))
+        num_people = num_people['number_of_people']
+        cursor.execute("INSERT INTO measures (station_name, count_people) VALUES (%s, %s)", (station, num_people))
         conn.commit()
+    print(datetime.datetime.now(), '| Information was sent')
     time.sleep(60)
 
 cursor.close()
